21ea7a00278b8e73d411b1bc3f35246b8e43f4cc
Luwei-Lin <luwei2@ualberta.ca>
2022-07-01 13:06:12 -0500

    update

--- PreprocessingData/tools/text_cleaner.py
+++ PreprocessingData/tools/text_cleaner.py
@@ -1,6 +1,8 @@
 import re
 import lxml.html.clean
 import unicodedata
+
+
 #from bs4 import BeautifulSoup
 '''
 Input: (String)  title, product_type, tags 
@@ -12,15 +14,16 @@ def clean_tags_text(raw_title, raw_product_type, raw_tags, raw_buckets):
     product_type = ''
     buckets = ''
     if isinstance(raw_tags, str) and raw_tags is not None:
-        tags = re.sub("-", " ", re.sub("'", " ",re.sub(",", " ", raw_tags)))
+        tags = re.sub("-", " ", re.sub("'", " ",re.sub(",", " ", raw_tags))).strip()
         #tags = raw_tags.lower()
     if isinstance(raw_title, str)and raw_title is not None:
-        title = re.sub("'", " ", re.sub("-", " ", re.sub(",", "", raw_title)))
+        title = re.sub("'", " ", re.sub("-", " ", re.sub(",", "", raw_title))).strip()
+        
         #title = raw_title.lower()
     if isinstance(raw_product_type, str) and raw_product_type is not None :
         product_type = re.sub(",", " ", raw_product_type)
     if isinstance(raw_buckets, str) and raw_buckets is not None:
-        buckets = re.sub("{", "", re.sub("}", "", re.sub(",", ", ", raw_buckets)))
+        buckets = re.sub("{", "", re.sub("}", "", re.sub(",", ", ", raw_buckets))).strip()
     
     reg_str = r"{(.*?)}"
     tags = re.sub('"', "", tags)#remove inside the sentence " signs for cleaning data
@@ -32,16 +35,15 @@ def clean_tags_text(raw_title, raw_product_type, raw_tags, raw_buckets):
     tags_toString = str(re.findall(reg_str, tags)).removeprefix('[').removesuffix(']')
     tags_toString = re.sub("'", "", re.sub('"', ' ', tags_toString))
     
-    contents += '; ' + product_type
-    
     if product_type != "":
-        contents += '; ' + tags_toString
-    else:
-        contents += tags_toString
+        contents += '. ' + product_type
         
-    
-    if buckets != '':
+    if tags_toString != "":
+        contents += ". " + tags_toString
+        
+    if buckets != "":
         contents += ". " + buckets 
+    
     return contents
 
 '''
@@ -52,7 +54,7 @@ Output: After processing body_html String
 '''
 def cleanHtml(html):
     cleantext = ""
-    if html is None:
+    if not isinstance(html, str) or html is None:
         return cleantext
     res = lxml.html.clean.clean_html(html)
     
@@ -72,10 +74,14 @@ def cleanHtml(html):
     
     cleantext = re.compile(r"\s+").sub(" ", contents).strip()
     cleantext = unicodedata.normalize("NFKD", cleantext)
-    
-    
+    try:
+        #discard the 
+        cleantext = cleantext.split("FEATURES")
+        return cleantext[0]
+    except:
+        return cleantext
     #clean_text = BeautifulSoup(text, "lxml").get_text(strip=True)
-    return cleantext
+    
 
 '''
 Directly use the information from csv cells in ['title', 'tags', 'body_html']
@@ -83,20 +89,28 @@ return pre-processing raw_text
 '''
 def raw_content(title, tags, body_html, buckets):
     
-    text_1 = clean_tags_text(title, None, tags, buckets)
+    text_1 = clean_tags_text(None, None, None, None)
     text_2 = cleanHtml(body_html)
 
     raw_text = text_1 + '. ' + text_2
-    return raw_text
-
+    res = re.compile(r"\s+").sub(" ", raw_text).strip()
+    return res
+'''
+Input: raw_html
+Function: extract special tag description from html
+Output: list
+'''
+def get_special_tag_product_description(raw_html):
+    tag = 'p' #if we just want to extract the content inside the <p> ..{content}. </p>
+    reg_str = '<' + tag + ">(.*?)</" + tag + '>'
+    cleanText = re.findall(reg_str, raw_html)
+    return cleanText
 def test():
     title = "Aria High Waist"
     tags = "{d-cup+,meta-size-chart-artesands-size-guide,moderate-coverage,one-piece,plus-size,swim-wear}"
     body_html = '"<meta charset="utf-8"><p><meta charset="utf-8"><span>The Hues Underwire One Piece has a beautifully sculpted feminine wrap-around style. The Hue has a hidden shelf bra with internal support underwire to fit a D cup to DD cup. This swimsuit style allows all of the fit and support required for body sculpting and figure forming confidence for the curvy body. </span></p><p data-mce-fragment="1"><meta charset="utf-8"><em data-mce-fragment="1">Model is wearing an Australian size 18+ </em></p><p class="p1" data-mce-fragment="1"><b data-mce-fragment="1">FEATURES:</b></p><ul class="ul1" data-mce-fragment="1"><li class="li1" data-mce-fragment="1">Supportive Underwire  <br data-mce-fragment="1"></li><li class="li1" data-mce-fragment="1">Removable &amp; Adjustable Straps  </li><li class="li1" data-mce-fragment="1">Front Ruching <br data-mce-fragment="1"></li><li class="li1" data-mce-fragment="1">D/DD Cup Underwire Support <br data-mce-fragment="1"></li><li class="li1" data-mce-fragment="1"> <div data-mce-fragment="1">Nylon / Elastane Blend. </div></li></ul><div class="p2" style="text-align: center;" data-mce-style="text-align: center;" data-mce-fragment="1"><b data-mce-fragment="1"><span data-mce-fragment="1">Artesands Fits Your Curves. Designed in Australia. </span></b></div><h5 style="text-align: left;" data-mce-fragment="1"><strong>Care Notes</strong></h5><p><strong></strong>We recommend hand washing in cold water. Dry flat in a shady spot out of direct sunlight. Do not wring out or hang dry. </p><p>Chlorinated water is not great and can cause colours to fade. Please make sure you always wash your swimsuit well after swimming in chlorinated water. We recommend swimsuit cleaner to keep your swimsuit looking amazing.</p>"'
     buckets = "{Casual,ActiveWear,Streetwear}"
     print(raw_content(title, tags, body_html, None))
-
-
 '''
 When you use the test() which used the raw_content() only. You can directly use the  
 Output:Aria High Waist; d cup+ meta size chart artesands size guide moderate coverage one piece plus size swim wear. The Hues Underwire One Piece has a beautifully sculpted feminine wrap-around style. The Hue has a hidden shelf bra with internal support underwire to fit a D cup to DD cup. This swimsuit style allows all of the fit and support required for body sculpting and figure forming confidence for the curvy body. Model is wearing an Australian size 18+ FEATURES:Supportive Underwire  Removable and; Adjustable Straps  Front Ruching D/DD Cup Underwire Support  Nylon / Elastane Blend. Artesands Fits Your Curves. Designed in Australia. Care NotesWe recommend hand washing in cold water. Dry flat in a shady spot out of direct sunlight. Do not wring out or hang dry. Chlorinated water is not great and can cause colours to fade. Please make sure you always wash your swimsuit well after swimming in chlorinated water. We recommend swimsuit cleaner to keep your swimsuit looking amazing.
